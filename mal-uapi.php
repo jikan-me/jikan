91ae467543ee01019fec7492afdce6ba4883501b
Nekomata <irfan.dahir1037@gmail.com>
2017-05-15 16:00:53 +0500

    update version

--- mal-uapi.php
+++ mal-uapi.php
@@ -1,6 +1,6 @@
 <?php
 /**
-*	MyAnimeList Unofficial API @version 0.1.2 alpha
+*	MyAnimeList Unofficial API @version 0.1.3 alpha
 *	Developed by Nekomata | irfandahir.com
 *	
 *	This is an unofficial MAL API that provides the features that the official one lacks.

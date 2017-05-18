62774744d5810ec6fdaaa5ed9d423eb3bc8ca773
Nekomata <irfan.dahir1037@gmail.com>
2017-05-18 21:46:29 +0500

    merge fix

--- example.php
+++ example.php
@@ -8,7 +8,7 @@ $firsts = array();
 
 $firsts['anime'] = $jikan->anime(1)->data; // get the anime with ID 1 on MAL
 $firsts['manga'] = $jikan->manga(1)->data; // get the manga with ID 1 on MAL
-$firsts['character'] = $mal->character(1)->data; // get the character with ID 1 on MAL
+$firsts['character'] = $jikan->character(1)->data; // get the character with ID 1 on MAL
 $firsts['person'] = $jikan->person(1)->data; // get the person with ID 1 on MAL
 
 var_dump($firsts); // data dump

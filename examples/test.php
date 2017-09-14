a886d2a6792908ac95d8d3e98664cf50c70ee63c
Nekomata <irfan.dahir1037@gmail.com>
2017-09-14 15:16:53 +0500

    updates

--- examples/test.php
+++ examples/test.php
@@ -5,10 +5,13 @@ $jikan = new Jikan\Jikan;
 
 
 $jikan->Anime(1);
-$jikan->Anime(1, [EPISODES, CHARACTERS_STAFF]);
-$jikan->Anime(21, [EPISODES=>2]);
-$jikan->Manga(1);
-$jikan->Manga(1, [CHARACTERS]);
-$jikan->Person(1);
-$jikan->Character(1);
+//$jikan->Anime(1, [EPISODES, CHARACTERS_STAFF]);
+//$jikan->Anime(21, [EPISODES=>2]);
+//$jikan->Manga(1);
+//$jikan->Manga(1, [CHARACTERS]);
+//$jikan->Person(1);
+//$jikan->Character(1);
+//
+//
 
+var_dump($jikan->response);

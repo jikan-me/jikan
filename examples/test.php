46602fde251b39cbb8dbaf9aae5982f23252e102
Nekomata <irfan.dahir1037@gmail.com>
2017-09-14 14:22:13 +0500

    fix travis

--- examples/test.php
+++ examples/test.php
@@ -3,6 +3,12 @@ require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 $jikan = new Jikan\Jikan;
 
-$jikan->Person(1868);
 
-var_dump($jikan->response['published_manga']);
+$jikan->Anime(1);
+$jikan->Anime(1, [EPISODES, CHARACTERS_STAFF]);
+$jikan->Anime(21, [EPISODES=>2]);
+$jikan->Manga(1);
+$jikan->Manga(1, [CHARACTERS]);
+$jikan->Person(1);
+$jikan->Character(1);
+

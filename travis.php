6cacf327d2f41aadd98673b742ad7d208db65b89
Nekomata <irfan.dahir1037@gmail.com>
2017-12-13 23:25:37 +0500

    update travis configurations

---
+++ travis.php
@@ -0,0 +1,23 @@
+<?php
+// build testing
+
+error_reporting(E_ALL);
+
+require_once dirname(__DIR__) . "/vendor/autoload.php"; 
+
+$jikan = new Jikan\Jikan;
+
+
+$jikan->Anime(21);
+sleep(5);
+$jikan->Anime(21, [EPISODES]);
+sleep(5);
+$jikan->Anime(21, [EPISODES, CHARACTERS_STAFF]);
+sleep(5);
+$jikan->Manga(1);
+sleep(5);
+$jikan->Manga(1, [CHARACTERS]);
+sleep(5);
+$jikan->Person(1);
+sleep(5);
+$jikan->Character(1);

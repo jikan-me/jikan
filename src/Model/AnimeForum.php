fd2511c82761a83c13461c3b501ca374d88d9e29
Nekomata <irfan.dahir1037@gmail.com>
2018-03-02 16:53:41 +0500

    added extended data Forum models

---
+++ src/Model/AnimeForum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jikan\Model;
+
+
+class AnimeForum extends Model
+{
+
+	public $topic = [];
+
+	public $topic_last_page = 1;
+
+}
---
+++ src/Model/MangaForum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jikan\Model;
+
+
+class MangaForum extends Model
+{
+
+	public $topic = [];
+
+	public $topic_last_page = 1;
+
+}

408f4a611638a40a9c565c6cb4b25b0fac5f5e19
irfan-dahir <irfan.dahir@gmail.com>
2018-03-22 09:56:46 +0500

    add query builder

--- src/Helper/SearchConfig.php
+++ src/Helper/SearchConfig.php
@@ -5,33 +5,66 @@ namespace Jikan\Helper;
 class SearchConfig {
 
 	private $type;
-	private $subtype;
-	private $score;
-	private $status;
-	private $rated;
+	private $subtype = 0;
+	private $score = 0;
+	private $status = 0;
+	private $rated = 0;
 	private $startDate = [];
 	private $endDate = [];
 	private $genreInclude = true; // Exclude = false
 	private $genre = [];
 
-	const $validTypes = [ANIME, MANGA];
-	const $validSubTypes = [TV, OVA, MOVIE, SPECIAL, ONA, MUSIC, MANGA, NOVEL, ONE_SHOT, DOUJINSHI, MANHWA, MANHUA, OEL];
-	const $validStatus = [AIRING, FINISHED_AIRING, TO_BE_AIRED, PUBLISHING, FINISHED_PUBLISHING, TO_BE_PUBLISHED];
-	const $validRating = [G, PG, PG13, R17, R, RX];
-	const $validGenre = [ACTION, ADVENTURE, CARS, COMEDY, DEMENTIA, DEMONS, MYSTERY, DRAMA, ECCHI, FANTASY, GAME, HENTAI, HISTORICAL, HORROR, KIDS, MAGIC, MARTIAL_ARTS, MECHA, MUSIC, PARODY, SAMURAI, ROMANCE, SCHOOL, SCI_FI, SHOUJO, SHOUJO_AI, SHOUNEN, SHOUNEN_AI, SPACE, SPORTS, SUPER_POWER, VAMPIRE, YAOI, YURI, HAREM, SLICE_OF_LIFE, SUPERNATURAL, MILITARY, POLICE, PSYCHOLOGICAL, THRILLER, SEINEN, JOSEI];
+	const VALID_TYPES = [ANIME, MANGA];
+	const VALID_SUB_TYPES = [TYPE_TV, TYPE_OVA, TYPE_MOVIE, TYPE_SPECIAL, TYPE_ONA, TYPE_MUSIC, TYPE_MANGA, TYPE_NOVEL, TYPE_ONE_SHOT, TYPE_DOUJINSHI, TYPE_MANHWA, TYPE_MANHUA, TYPE_OEL];
+	const VALID_STATUS = [AIRING, FINISHED_AIRING, TO_BE_AIRED, PUBLISHING, FINISHED_PUBLISHING, TO_BE_PUBLISHED];
+	const VALID_RATING = [G, PG, PG13, R17, R, RX];
+	const VALID_GENRE = [ACTION, ADVENTURE, CARS, COMEDY, DEMENTIA, DEMONS, MYSTERY, DRAMA, ECCHI, FANTASY, GAME, HENTAI, HISTORICAL, HORROR, KIDS, MAGIC, MARTIAL_ARTS, MECHA, MUSIC, PARODY, SAMURAI, ROMANCE, SCHOOL, SCI_FI, SHOUJO, SHOUJO_AI, SHOUNEN, SHOUNEN_AI, SPACE, SPORTS, SUPER_POWER, VAMPIRE, YAOI, YURI, HAREM, SLICE_OF_LIFE, SUPERNATURAL, MILITARY, POLICE, PSYCHOLOGICAL, THRILLER, SEINEN, JOSEI];
 
 	public function __construct($type) {
-		$this->type = in_array($type, $validTypes) ? $type : throw new Exception("Failed to build Search Config, unsupported type. (Supported: ANIME, MANGA)");
+		if (!in_array($type, self::VALID_TYPES)) {
+			throw new \Exception("Failed to build Search Config, unsupported type. (Supported: ANIME, MANGA)");
+		}
+		$this->type = $type;
 
 		return $this;
 	}
 
-	public function buildQuery() {
-		return "";
+	public function build() {
+		$query = "";
+		// Type
+		$query .= "type=".$this->subType;
+		// Score
+		$query .= "&score=".$this->score;
+		// Status
+		$query .= "&status=".$this->status;
+		// Rated
+		$query .= "&r=".$this->rated;
+		// Start Date
+		if (!empty($this->startDate)) {
+			$query .= "&sd=".$this->startDate[0];
+			$query .= "&sm=".$this->startDate[1];
+			$query .= "&sy=".$this->startDate[2];
+		}
+		// End Date
+		if (!empty($this->endDate)) {
+			$query .= "&ed=".$this->endDate[0];
+			$query .= "&em=".$this->endDate[1];
+			$query .= "&ey=".$this->endDate[2];
+		}
+		// Genre Include
+		$query .= "&gx=".($this->genreInclude ? "0" : "1");
+		// Genre
+		if (!empty($this->genre)) {
+			foreach ($this->genre as $key => $genre) {
+				$query .= "&genre[]=".$genre;
+			}
+		}
+
+		return $query;
 	}
 
-	public function setSubType($subType) {
-		$this->subType = in_array($subType, $validSubTypes) ? $subType : 0;
+	public function setType($subType) {
+		$this->subType = in_array($subType, self::VALID_SUB_TYPES) ? $subType : 0;
 
 		return $this;
 	}
@@ -43,13 +76,13 @@ class SearchConfig {
 	}
 
 	public function setStatus($status) {
-		$this->status = in_array($status, $validStatus) ? $status : 0;
+		$this->status = in_array($status, self::VALID_STATUS) ? $status : 0;
 
 		return $this;
 	}
 
 	public function setRated($rated) {
-		$this->rated = in_array($rated, $validRating) ? $rated : 0;
+		$this->rated = in_array($rated, self::VALID_RATING) ? $rated : 0;
 
 		return $this;
 	}

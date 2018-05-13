1161cb6249c4b87b4b61114488ea830d87dc2f53
irfan-dahir <irfan.dahir@gmail.com>
2018-05-14 02:51:27 +0500

    fix genreInclude

--- src/Helper/SearchConfig.php
+++ src/Helper/SearchConfig.php
@@ -48,7 +48,7 @@ class SearchConfig {
 		$query .= "&em=".$this->endDate[1];
 		$query .= "&ed=".$this->endDate[2];
 		// Genre Include
-		$query .= "&gx=".($this->genreInclude ? "1" : "0");
+		$query .= "&gx=".($this->genreInclude ? "0" : "1");
 		// Genre
 		if (!empty($this->genre)) {
 			foreach ($this->genre as $key => $genre) {

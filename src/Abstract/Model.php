2ac2d420e69a0bd19873f399e9da59412a645f8d
irfan-dahir <irfan.dahir@gmail.com>
2018-06-20 17:34:34 +0500

    add Model abstract class

---
+++ src/Abstract/Model.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jikan\Abstract;
+
+abstract class Model
+{
+
+	public function set($class, $key, $value) {
+		if (property_exists('Jikan\Model\\'.$class, $key)) {
+			$this->{$key} = $value;
+		}
+	}
+
+	public function get($class, $key) {
+		if (property_exists('Jikan\Model\\'.$class, $key)) {
+			return $this->{$key};
+		}
+	}
+
+}

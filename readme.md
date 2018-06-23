39c28adf496535a036c6c7290c1012f738a253df
irfan-dahir <irfan.dahir@gmail.com>
2018-06-24 03:29:17 +0500

    update

--- readme.md
+++ readme.md
@@ -5,3 +5,48 @@ Bleeding edge, re-write of the Jikan PHP API. Focusing on speed, stability and c
 
 ## Dependency
 - [irfan-dahir/skraypar](https://github.com/irfan-dahir/skraypar)
+
+## Refactoring Process
+### Phase 1
+- [X] **Core**
+- [X] Modal Class
+- Data Response enhancement methods [#147](/../../issues/147), [#153](/../../issues/153)
+	- [ ] HTML quotes
+	- [ ] Trims
+	- [ ] UTF8 Encodes
+	- [ ] Null for empty
+
+### Phase 2
+- Anime
+	- [ ] Main
+	- [ ] Characters & Staff
+	- [ ] Episodes
+	- [ ] News
+	- [ ] Videos
+	- [ ] Stats
+	- [ ] Pictures
+	- [ ] Forum
+	- [ ] More Info
+- Manga
+	- [ ] Main
+	- [ ] Characters
+	- [ ] News
+	- [ ] Stats
+	- [ ] Pictures
+	- [ ] Forum
+	- [ ] More Info
+- Person
+	- [ ] Main
+	- [ ] Pictures
+- Character
+	- [ ] Main
+	- [ ] Pictures
+- [ ] Schedule
+- Search
+	- [ ] Basic
+	- [ ] Advanced
+- [ ] Seasonal
+- [ ] Top
+
+### Phase 3
+- [ ] Code Clean Up
--- travis.php
+++ travis.php
@@ -7,21 +7,27 @@ require_once "vendor/autoload.php";
 
 $jikan = new Jikan\Jikan;
 
+/*
+ * Anime Tests
+ */
+try {
+	$jikan->Anime(
+		(new Jikan\Request\Anime())->setID(1)
+	);
 
-$jikan->Anime(21);
-sleep(5);
-$jikan->Anime(21, [EPISODES]);
-sleep(5);
-$jikan->Anime(21, [CHARACTERS_STAFF]);
-sleep(5);
-$jikan->Anime(21, [NEWS]);
-sleep(5);
-$jikan->Manga(1);
-sleep(5);
-$jikan->Manga(1, [CHARACTERS]);
-sleep(5);
-$jikan->Manga(1, [NEWS]);
-sleep(5);
-$jikan->Person(1);
-sleep(5);
-$jikan->Character(1);
+	var_dump($jikan->response);
+} catch (Exception $e) {
+	var_dump("STATUS: " . $jikan->request->parser->status);
+	var_dump("ERROR: " . $e->getMessage());
+}
+sleep(4);
+try {
+	$jikan->Anime(
+		(new Jikan\Request\Anime(CHARACTERS_STAFF))->setID(1)
+	);
+
+	var_dump($jikan->response);
+} catch (Exception $e) {
+	var_dump("STATUS: " . $jikan->request->parser->status);
+	var_dump("ERROR: " . $e->getMessage());
+}

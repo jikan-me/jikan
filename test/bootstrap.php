969cfde6064b4e97faf634491bc4b2595fbf913f
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2018-06-24 13:56:04 +0200

    Improve request matchers

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,5 +3,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 \VCR\VCR::configure()
     ->setCassettePath('test/fixtures')
+    ->enableRequestMatchers(['url', 'method'])
 ;
 \VCR\VCR::turnOn();

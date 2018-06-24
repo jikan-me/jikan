225cba53e6ef373105df823bffaf2504e3409c4c
irfan-dahir <irfan.dahir@gmail.com>
2018-06-24 21:10:06 +0500

    update helper class

--- src/Abstracts/Helper.php
+++ src/Abstracts/Helper.php
@@ -5,8 +5,13 @@ namespace Jikan\Abstracts;
 abstract class Helper extends \Jikan\Abstracts\Container
 {
 
+	public function __construct() {
+		return $this;
+	}
+
 	public function set($key, $value) {
 		$this->offsetSet($key, $value);
+		return $this;
 	}
 
 	public function get($key) {

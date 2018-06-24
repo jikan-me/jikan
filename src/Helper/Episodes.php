1beda47732842d8aaf81e4e430827a666c64b473
irfan-dahir <irfan.dahir@gmail.com>
2018-06-24 21:10:15 +0500

    update episodes helper

--- src/Helper/Episodes.php
+++ src/Helper/Episodes.php
@@ -4,6 +4,14 @@ namespace Jikan\Helper;
 
 class Episodes extends \Jikan\Abstracts\Helper
 {
+
+	public function __construct() {
+	}
+
+	public function setPage(int $page) {
+		$this->offsetSet('p', $page);
+	}
+
 	public function build() {
 		$query = "";
 		foreach ($this->container as $key => $value) {

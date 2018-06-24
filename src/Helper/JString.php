50ff4173965281cd6ed48612971b7e8e61ca7991
irfan-dahir <irfan.dahir@gmail.com>
2018-06-24 07:47:49 +0500

    add assertNull

--- src/Helper/JString.php
+++ src/Helper/JString.php
@@ -7,12 +7,18 @@ class JString
 
 	public static function cleanse(string $string) : string {
 		return trim(
-			htmlspecialchars_decode($string, ENT_QUOTES)
+			htmlspecialchars_decode(
+				strip_tags($string), 
+			ENT_QUOTES)
 		);
 	}
 
 	public static function utf8(string $string) : string {
 		return utf8_encode($string);
+	}
+
+	public static function assertNull(string $string) {
+		return empty($string) ? null : $string;
 	}
 
 }

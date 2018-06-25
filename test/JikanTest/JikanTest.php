21598bbbfb276ccff6691fbc62ba51f06af01a69
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2018-06-25 14:58:36 +0200

    Add top level test

--- test/JikanTest/JikanTest.php
+++ test/JikanTest/JikanTest.php
@@ -27,4 +27,16 @@ class JikanTest extends TestCase
         $anime = $this->jikan->Anime(new \Jikan\Request\Anime(21));
         self::assertInstanceOf(\Jikan\Model\Anime::class, $anime);
     }
+
+    /**
+     * @test
+     * @vcr SeasonalParserTest.yaml
+     */
+    public function it_gets_seasonal_anime()
+    {
+        $seasonal = $this->jikan->getSeasonal(new \Jikan\Request\Seasonal(2018, 'summer'));
+        self::assertInstanceOf(\Jikan\Model\Seasonal::class, $seasonal);
+        self::assertCount(91, $seasonal->getAnime());
+        self::assertContainsOnlyInstancesOf(\Jikan\Model\SeasonalAnime::class, $seasonal->getAnime());
+    }
 }

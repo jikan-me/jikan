0ace5735c3f3b13e46a2df86a85365cd8c6da9b9
irfan-dahir <irfan.dahir@gmail.com>
2018-06-27 00:56:32 +0500

    add manga model

--- src/Model/Manga.php
+++ src/Model/Manga.php
@@ -6,56 +6,273 @@ namespace Jikan\Model;
 class Manga extends Model
 {
 
-	public $mal_id;
+	private $mal_id;
 	
-	public $link_canonical;
+	private $url;
 
-	public $title;
+	private $title;
 
-	public $title_english;
+	private $title_english;
 
-	public $title_synonyms;
+	private $title_synonyms;
 
-	public $title_japanese;
+	private $title_japanese;
 
-	public $status;
+	private $status;
 
-	public $image_url;
+	private $image_url;
 
-	public $type;
+	private $type;
 
-	public $volumes;
+	private $volumes;
 
-	public $chapters;
+	private $chapters;
 
-	public $publishing = false;
+	private $publishing = false;
 
-	public $published_string;
+	private $published_string;
 
-	public $published = [];
+	private $published = [];
 
-	public $rank;
+	private $rank;
 
-	public $score;
+	private $score;
 
-	public $scored_by;
+	private $scored_by;
 
-	public $popularity;
+	private $popularity;
 
-	public $members;
+	private $members;
 
-	public $favorites;
+	private $favorites;
 
-	public $synopsis;
+	private $synopsis;
 
-	public $background;
+	private $background;
 	
-	public $related = [];
+	private $related = [];
 
-	public $genre = [];
+	private $genre = [];
 
-	public $author = [];
+	private $author = [];
 
-	public $serialization = [];
+	private $serialization = [];
 
+
+	/**
+     * Create an instance from an Manga parser
+     *
+     * @param \Jikan\Parser\Manga $parser
+     *
+     * @return Manga
+     */
+    public static function fromParser(\Jikan\Parser\Manga $parser): Manga
+    {
+        $instance = new self();
+
+
+        return $instance;
+	}
+	
+    /**
+     * @return int
+     */
+    public function getMalId(): int
+    {
+        return $this->mal_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitleEnglish(): string
+    {
+        return $this->title_english;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitleJapanese(): string
+    {
+        return $this->title_japanese;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitleSynonyms(): string
+    {
+        return $this->title_synonyms;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageUrl(): string
+    {
+        return $this->image_url;
+	}
+	
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getVolumes(): int
+    {
+        return $this->volumes;
+	}
+
+    /**
+     * @return int
+     */
+    public function getChapters(): int
+    {
+        return $this->chapters;
+	}
+	
+    /**
+     * @return bool
+     */
+    public function isPublishing(): bool
+    {
+        return $this->publishing;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublishedString(): string
+    {
+        return $this->published_string;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPublished(): array
+    {
+        return $this->published;
+    }	
+
+
+    /**
+     * @return string
+     */
+    public function getScore(): string
+    {
+        return $this->score;
+    }
+
+    /**
+     * @return string
+     */
+    public function getScoredBy(): string
+    {
+        return $this->scored_by;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRank(): string
+    {
+        return $this->rank;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPopularity(): string
+    {
+        return $this->popularity;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMembers(): string
+    {
+        return $this->members;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFavorites(): string
+    {
+        return $this->favorites;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSynopsis(): string
+    {
+        return $this->synopsis;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBackground(): string
+    {
+        return $this->background;
+	}
+	
+    /**
+     * @return array
+     */
+    public function getRelated(): array
+    {
+        return $this->related;
+	}
+	
+    /**
+     * @return array
+     */
+    public function getGenre(): array
+    {
+        return $this->genre;
+	}
+	
+    /**
+     * @return array
+     */
+    public function getAuthor(): array
+    {
+        return $this->author;
+	}
+	
+    /**
+     * @return array
+     */
+    public function getSerialization(): array
+    {
+        return $this->serialization;
+    }
 }

412c31e38686d975bb0a43b3b7b986c586400235
irfan-dahir <irfan.dahir@gmail.com>
2018-06-27 22:41:58 +0500

    add Person Request

--- src/Request/Person.php
+++ src/Request/Person.php
@@ -2,7 +2,35 @@
 
 namespace Jikan\Request;
 
-class Person extends Requests
+/**
+ * Class Person
+ *
+ * @package Jikan\Request
+ */
+class Person implements RequestInterface
 {
-	
+    /**
+     * @var int
+     */
+    private $personId;
+
+    /**
+     * Person constructor.
+     *
+     * @param int $personId
+     */
+    public function __construct($personId)
+    {
+        $this->personId = $personId;
+    }
+
+    /**
+     * Get the path to request
+     *
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return sprintf('https://myanimelist.net/person/%s', $this->personId);
+    }
 }

e8ce90443e4d17921b9878a493252a9bf538d173
irfan-dahir <irfan.dahir@gmail.com>
2018-06-27 22:49:16 +0500

    add instance and getters

--- src/Model/Person.php
+++ src/Model/Person.php
@@ -9,6 +9,10 @@ class Person extends Model
 
 	public $url;
 
+	public $imageUrl;
+
+	public $websiteUrl;
+
 	public $name;
 
 	public $givenName;
@@ -19,13 +23,9 @@ class Person extends Model
 
 	public $birthday;
 
-	public $websiteUrl;
-
 	public $memberFavorites;
 
 	public $more;
-
-	public $imageUrl;
 
 	public $voiceActingRole = [];
 
@@ -33,4 +33,130 @@ class Person extends Model
 
 	public $publishedManga = [];
 
+
+    /**
+     * @param Parser\Person $parser
+     *
+     * @return Person
+     */
+    public static function fromParser(Parser\Person $parser): self
+    {
+        $instance = new self();
+
+        return $instance;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMalId(): int
+    {
+        return $this->malId;
+	}
+	
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+	}
+
+    /**
+     * @return string
+     */
+    public function getWebsiteUrl(): string
+    {
+        return $this->websiteUrl;
+	}
+	
+
+    /**
+     * @return string
+     */
+    public function getImageUrl(): string
+    {
+        return $this->imageUrl;
+    }
+	
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+	}
+	
+    /**
+     * @return string
+     */
+    public function getGivenName(): string
+    {
+        return $this->givenName;
+	}
+	
+    /**
+     * @return string
+     */
+    public function getFamilyName(): string
+    {
+        return $this->familyName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlternateName(): string
+    {
+        return $this->alternateName;
+	}
+
+    /**
+     * @return string
+     */
+    public function getBirthday(): string
+    {
+        return $this->birthday;
+	}
+
+    /**
+     * @return string
+     */
+    public function getMemberFavorites(): string
+    {
+        return $this->memberFavorites;
+	}
+
+    /**
+     * @return string
+     */
+    public function getMore(): string
+    {
+        return $this->more;
+	}
+
+    /**
+     * @return array
+     */
+    public function getVoiceActingRole(): array
+    {
+        return $this->voiceActingRole;
+	}
+
+    /**
+     * @return array
+     */
+    public function getAnimeStaffPosition(): array
+    {
+        return $this->animeStaffPosition;
+	}
+
+    /**
+     * @return array
+     */
+    public function getPublishedManga(): array
+    {
+        return $this->publishedManga;
+	}
+	
 }

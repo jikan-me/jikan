0da552ff37da3937efecc862b94ac4f8576024dc
irfan-dahir <irfan.dahir@gmail.com>
2018-06-29 00:14:22 +0500

    add AnimeStats sub parser

---
+++ src/Model/AnimeStats.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Jikan\Model;
+
+/**
+ * Class AnimeStats
+ *
+ * @package Jikan\Model
+ */
+class AnimeStats
+{
+    /**
+     * @var float
+     */
+    private $daysWatched;
+
+    /**
+     * @var float
+     */
+    private $meanScore;
+
+    /**
+     * @var int
+     */
+    private $watching;
+
+    /**
+     * @var int
+     */
+    private $completed;
+
+    /**
+     * @var int
+     */
+    private $onHold;
+
+    /**
+     * @var int
+     */
+    private $dropped;
+
+    /**
+     * @var int
+     */
+    private $planToWatch;
+
+    /**
+     * @var int
+     */
+    private $totalEntries;
+
+    /**
+     * @var int
+     */
+    private $rewatched;
+
+    /**
+     * @var int
+     */
+    private $episodesWatched;
+
+    /**
+     * @param Parser\AnimeStats $parser
+     *
+     * @return AnimeStats
+     */
+    public static function fromParser(\Jikan\Parser\AnimeStats $parser): self
+    {
+        $instance = new self();
+        $instance->daysWatched = $parser->getDaysWatched();
+        $instance->meanScore = $parser->getMeanScore();
+        $instance->watching = $parser->getWatching();
+        $instance->completed = $parser->getCompleted();
+        $instance->onHold = $parser->getOnHold();
+        $instance->dropped = $parser->getDropped();
+        $instance->planToWatch = $parser->getPlanToWatch();
+        $instance->totalEntries = $parser->getTotalEntries();
+        $instance->rewatched = $parser->getRewatched();
+        $instance->episodesWatched = $parser->getEpisodesWatched();
+
+
+        return $instance;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDaysWatched() : float {
+        return $this->daysWatched;
+    } 
+
+    /**
+     * @return float
+     */
+    public function getMeanScore() : float {
+        return $this->meanScore;
+    } 
+
+    /**
+     * @return int
+     */
+    public function getWatching() : int {
+        return $this->watching;
+    } 
+
+    /**
+     * @return int
+     */
+    public function getCompleted() : int {
+        return $this->completed;
+    } 
+
+    /**
+     * @return int
+     */
+    public function getOnHold() : int {
+        return $this->onHold;
+    } 
+
+    /**
+     * @return int
+     */
+    public function getDropped() : int {
+        return $this->dropped;
+    } 
+
+    /**
+     * @return int
+     */
+    public function getPlanToWatch() : int {
+        return $this->planToWatch;
+    } 
+
+    /**
+     * @return int
+     */
+    public function getTotalEntries() : int {
+        return $this->totalEntries;
+    } 
+
+    /**
+     * @return int
+     */
+    public function getRewatched() : int {
+        return $this->rewatched;
+    } 
+
+    /**
+     * @return int
+     */
+    public function getEpisodesWatched() : int {
+        return $this->episodesWatched;
+    } 
+}

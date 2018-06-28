d39cbbe142471ea95dfb75e4490e57c0f302ddf3
irfan-dahir <irfan.dahir@gmail.com>
2018-06-29 00:47:14 +0500

    add mangastats

---
+++ src/Parser/MangaStats.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace Jikan\Parser;
+
+use Jikan\Helper\JString;
+use Jikan\Helper\Parser;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Class MangaStats
+ *
+ * @package Jikan\Parser
+ */
+class MangaStats
+{
+    /**
+     * @var Crawler
+     */
+    private $crawler;
+
+    /**
+     * Animeograpy constructor.
+     *
+     * @param Crawler $crawler
+     */
+    public function __construct(Crawler $crawler)
+    {
+        $this->crawler = $crawler;
+    }
+
+    /**
+     * @return \Jikan\Model\MangaStats
+     */
+    public function getModel(): \Jikan\Model\MangaStats
+    {
+        return \Jikan\Model\MangaStats::fromParser($this);
+    }
+
+    /**
+     * @return float|null
+     * @throws \InvalidArgumentException
+     */
+    public function getDaysRead(): ?float
+    {
+        $node = $this->crawler
+            ->filterXPath('//div[@class=\'di-tc al pl8 fs12 fw-b\'][2]');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return Parser::removeChildNodes($node)->text();
+    }
+
+    /**
+     * @return float|null
+     * @throws \InvalidArgumentException
+     */
+    public function getMeanScore(): ?float
+    {
+        $node = $this->crawler
+            ->filterXPath('//div[@class=\'di-tc ar pr8 fs12 fw-b\'][2]');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return Parser::removeChildNodes($node)->text();
+    }
+
+
+    /**
+     * @return int|null
+     * @throws \InvalidArgumentException
+     */
+    public function getReading(): ?int
+    {
+        $node = $this->crawler
+            ->filterXPath('//a[contains(@class, \'reading\')]/following-sibling::span');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return $node->text();
+    }
+
+    /**
+     * @return int|null
+     * @throws \InvalidArgumentException
+     */
+    public function getCompleted(): ?int
+    {
+        $node = $this->crawler
+            ->filterXPath('//a[contains(@class, \'completed\')][2]/following-sibling::span');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return $node->text();
+    }
+
+    /**
+     * @return int|null
+     * @throws \InvalidArgumentException
+     */
+    public function getOnHold(): ?int
+    {
+        $node = $this->crawler
+            ->filterXPath('//a[contains(@class, \'on_hold\')][2]/following-sibling::span');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return $node->text();
+    }
+
+    /**
+     * @return int|null
+     * @throws \InvalidArgumentException
+     */
+    public function getDropped(): ?int
+    {
+        $node = $this->crawler
+            ->filterXPath('//a[contains(@class, \'dropped\')][2]/following-sibling::span');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return $node->text();
+    }
+
+    /**
+     * @return int|null
+     * @throws \InvalidArgumentException
+     */
+    public function getPlanToRead(): ?int
+    {
+        $node = $this->crawler
+            ->filterXPath('//a[contains(@class, \'plan_to_read\')]/following-sibling::span');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return $node->text();
+    }
+
+    /**
+     * @return int|null
+     * @throws \InvalidArgumentException
+     */
+    public function getTotalEntries(): ?int
+    {
+        $node = $this->crawler
+            ->filterXPath('//ul[@class=\'stats-data fl-r\'][2]/li[1]/span[2]');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return $node->text();
+    }
+
+    /**
+     * @return int|null
+     * @throws \InvalidArgumentException
+     */
+    public function getReread(): ?int
+    {
+        $node = $this->crawler
+            ->filterXPath('//ul[@class=\'stats-data fl-r\'][2]/li[2]/span[2]');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return $node->text();
+    }
+
+    /**
+     * @return int|null
+     * @throws \InvalidArgumentException
+     */
+    public function getChaptersRead(): ?int
+    {
+        $node = $this->crawler
+            ->filterXPath('//ul[@class=\'stats-data fl-r\'][2]/li[3]/span[2]');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return str_replace(',', '', $node->text());
+    }
+
+    /**
+     * @return int|null
+     * @throws \InvalidArgumentException
+     */
+    public function getVolumesRead(): ?int
+    {
+        $node = $this->crawler
+            ->filterXPath('//ul[@class=\'stats-data fl-r\'][2]/li[4]/span[2]');
+
+        if (!$node->count()) {
+            return null;
+        }
+
+        return str_replace(',', '', $node->text());
+    }
+}

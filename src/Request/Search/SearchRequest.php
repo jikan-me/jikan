a6c3f0b67f67bdc31794fb4cd8ac683ba036b831
irfan-dahir <irfan.dahir@gmail.com>
2018-07-07 19:29:25 +0500

    updates

--- src/Request/Search/SearchRequest.php
+++ src/Request/Search/SearchRequest.php
@@ -35,6 +35,55 @@ class SearchRequest implements RequestInterface
     private $page;
 
     /**
+     * Advanced Search
+     */
+
+    /**
+     * @var string
+     */
+    private $subType = 0;
+
+    /**
+     * @var float
+     */
+    private $score = 0;
+
+    /**
+     * @var int
+     */
+    private $status = 0;
+
+    /**
+     * @var int
+     */
+    private $producer = 0;
+
+    /**
+     * @var int
+     */
+    private $rated = 0;
+
+    /**
+     * @var int[]
+     */
+    private $startDate = [0,0,0];
+
+    /**
+     * @var int[]
+     */
+    private $endDate = [0,0,0];
+
+    /**
+     * @var int[]
+     */
+    private $genre = [];
+
+    /**
+     * @var bool
+     */
+    private $genreExclude = false;
+
+    /**
      * SearchRequest constructor.
      *
      * @param string $query
@@ -63,9 +112,105 @@ class SearchRequest implements RequestInterface
 
         $query = http_build_query([
             'q' => $this->query,
-            'show' => $this->page ? 50 * $this->page : null
+            'show' => $this->page ? 50 * $this->page : null,
+            'type' => $this->subType,
+            'score' => $this->score,
+            'status' => $this->status,
+            'p' => $this->producer,
+            'r' => $this->rated,
+            'sd' => $this->startDate[0],
+            'sm' => $this->startDate[1],
+            'sy' => $this->startDate[2],
+            'ed' => $this->endDate[0],
+            'em' => $this->endDate[1],
+            'ey' => $this->endDate[2],
+            'gx' => (int) $this->genreExclude
         ]);
 
-        return sprintf('https://myanimelist.net/%s.php?%s&c[]=a&c[]=b&c[]=c&c[]=f&c[]=d&c[]=e&c[]=g', $this->type, $query);
+
+        return sprintf(
+            'https://myanimelist.net/%s.php?%s&c[]=a&c[]=b&c[]=c&c[]=f&c[]=d&c[]=e&c[]=g',
+            $this->type,
+            $query
+        );
+    }
+
+    public function setSubType(string $subType)
+    {
+        $this->subType = $subType;
+    }
+
+    /**
+     * @param float $score
+     */
+    public function setScore(float $score)
+    {
+        $this->score = $score;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatus(int $status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @param int $producer
+     */
+    public function setProducer(int $producer)
+    {
+        $this->producer = $producer;
+    }
+
+    /**
+     * @param int $rated
+     */
+    public function setRated(int $rated)
+    {
+        $this->rated = $rated;
+    }
+
+    /**
+     * @param int[] $startDate
+     */
+    public function setStartDate(array $startDate)
+    {
+        $this->startDate = $startDate;
+    }
+
+    /**
+     * @param int[] $endDate
+     */
+    public function setEndDate(array $endDate)
+    {
+        $this->endDate = $endDate;
+    }
+
+    /**
+     * @param int[] $genre
+     */
+    public function setGenre($genre)
+    {
+        if (is_array($genre)) {
+            $this->genre = array_unique(
+                array_merge($genre, $this->genre)
+            );
+
+            return;
+        }
+
+        if (!in_array($genre, $this->genre)) {
+            $this->genre[] = $genre;
+        }
+    }
+
+    /**
+     * @param bool $genreExclude
+     */
+    public function setGenreExclude(bool $genreExclude)
+    {
+        $this->genreExclude = $genreExclude;
     }
 }

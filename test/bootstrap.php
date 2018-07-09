906caf5f04331fb91ab81b5e1c130414caa3a83c
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2018-07-09 18:06:12 +0200

    Only enable curl hook

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 \VCR\VCR::configure()
     ->setCassettePath('test/fixtures')
+    ->enableLibraryHooks(['curl'])
     ->enableRequestMatchers(['url', 'method', 'query_string'])
 ;
 \VCR\VCR::turnOn();

d9bae15cdf56bb4a1e6e611dabcf0580ba80bcde
Irfan <irfan.dahir@gmail.com>
2018-07-10 10:16:48 +0500

    add moreinfo request

---
+++ src/Request/Anime/AnimeMoreInfoRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Jikan\Request\Anime;
+
+use Jikan\Request\RequestInterface;
+
+/**
+ * Class AnimeMoreInfoRequest
+ *
+ * @package Jikan\Request\Anime
+ */
+class AnimeMoreInfoRequest implements RequestInterface
+{
+
+}

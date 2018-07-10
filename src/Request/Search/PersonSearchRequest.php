8761b07996292a365c4c3f6dae88953b9ad2fbd2
irfan-dahir <irfan.dahir@gmail.com>
2018-07-10 21:59:24 +0500

    add PersonSearchRequest

---
+++ src/Request/Search/PersonSearchRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Jikan\Request\Search;
+
+use Jikan\Request\RequestInterface;
+
+/**
+ * Class PersonSearchRequest
+ *
+ *
+ * @package Jikan\Request\Search
+ */
+class PersonSearchRequest implements RequestInterface
+{
+
+    /**
+     * @var string
+     */
+    private $query;
+
+    /**
+     * @var int
+     */
+    private $page;
+
+    /**
+     * SearchRequest constructor.
+     *
+     * @param string $query
+     * @param int    $page
+     */
+    public function __construct(string $query, int $page = 1)
+    {
+        $this->query = $query;
+        $this->page = $page;
+    }
+
+    /**
+     * Get the path to request
+     *
+     * @return string
+     */
+    public function getPath(): string
+    {
+
+        $query = http_build_query([
+            'q' => $this->query,
+            'show' => ($this->page !== 1) ? 50 * ($this->page-1) : null,
+        ]);
+
+        return sprintf(
+            'https://myanimelist.net/character.php?%s',
+            $query
+        );
+    }
+}

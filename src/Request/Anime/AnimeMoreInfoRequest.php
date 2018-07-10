5d70abd77e792465c398015d5dcb480564adc6f8
Irfan <irfan.dahir@gmail.com>
2018-07-10 09:27:49 +0500

    add  PR #167

---
+++ src/Request/Anime/AnimeMoreInfoRequest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Jikan\Request\Anime;
+use Jikan\Request\RequestInterface;
+/**
+ * Class AnimeMoreInfoRequest
+ *
+ * @package Jikan\Request\Anime
+ */
+class AnimeMoreInfoRequest implements RequestInterface
+{
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * AnimeRequest constructor.
+     *
+     * @param int $id
+     */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+    /**
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return sprintf('https://myanimelist.net/anime/%d/_/moreinfo', $this->id);
+    }
+}

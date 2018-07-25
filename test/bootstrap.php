6d2b72d28d52875de1617a4e14b4f074a73b0ac6
irfan-dahir <irfan.dahir@gmail.com>
2018-07-25 18:32:49 +0500

    separate fixtures #65

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,7 +1,16 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+
+if (!file_exists('jikan-fixtures/fixtures')) {
+    echo "ERROR: jikan-fixtures/fixtures not found\n";
+    echo "Clone the fixtures repo into project root directory\n";
+    echo "git clone https://github.com/jikan-me/jikan-fixtures.git\n";
+    die;
+}
+
+
 \VCR\VCR::configure()
-    ->setCassettePath('test/fixtures')
+    ->setCassettePath('jikan-fixtures/fixtures')
     ->enableLibraryHooks(['curl'])
     ->enableRequestMatchers(['url', 'method', 'query_string'])
 ;

8bdf8c3a059b674b7b574d5954e523db57f86308
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2019-03-09 16:42:26 +0100

    Add a failing test

---
+++ test/JikanTest/Parser/Search/AnimeSearchAiringTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace JikanTest\Parser\Search;
+
+use Jikan\MyAnimeList\MalClient;
+use Jikan\Request\Search\AnimeSearchRequest;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AnimeSearchTest
+ */
+class AnimeSearchAiringTest extends TestCase
+{
+    /**
+     * @test
+     * @vcr AnimeSearchAiringTest.yaml
+     */
+    public function it_gets_airing_non_null()
+    {
+        $jikan = new MalClient;
+        $this->search = $jikan->getAnimeSearch(new AnimeSearchRequest('Kaguya'));
+        $anime = $this->search->getResults()[0];
+        self::assertEquals('Kaguya-sama wa Kokurasetai: Tensai-tachi no Renai Zunousen', $anime->getTitle());
+        self::assertTrue($anime->isAiring());
+    }
+
+    /**
+     * @test
+     * @vcr AnimeSearchAiringTest.yaml
+     */
+    public function it_gets_airing_null()
+    {
+        $jikan = new MalClient;
+        $this->search = $jikan->getAnimeSearch(new AnimeSearchRequest('Aikatsu Friends'));
+        $anime = $this->search->getResults()[0];
+        self::assertEquals('Aikatsu Friends!', $anime->getTitle());
+        self::assertTrue($anime->isAiring());
+    }
+}

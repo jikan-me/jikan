81a77ab8807e74d5152327725933dd1118a24c3e
Irfan <irfan.dahir@gmaill.com>
2020-04-23 17:34:15 +0500

    wip fix [ci skip]

--- src/Model/Watch/PromotionalVideos.php
+++ src/Model/Watch/PromotionalVideos.php
@@ -2,6 +2,7 @@
 
 namespace Jikan\Model\Watch;
 
+use Jikan\Model\Common\Collection\Pagination;
 use Jikan\Model\Common\Collection\Results;
 use Jikan\Parser\Watch\WatchPromotionalVideosParser;
 
@@ -10,7 +11,7 @@ use Jikan\Parser\Watch\WatchPromotionalVideosParser;
  *
  * @package Jikan\Model
  */
-class PromotionalVideos extends Results
+class PromotionalVideos extends Results implements Pagination
 {
     /**
      * @param WatchPromotionalVideosParser $parser
@@ -24,5 +25,29 @@ class PromotionalVideos extends Results
         $instance->results = $parser->getResults();
 
         return $instance;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasNextPage(): bool
+    {
+        return $this->hasNextPage;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getLastVisiblePage(): ?int
+    {
+        return $this->lastVisiblePage;
+    }
+
+    /**
+     * @return array
+     */
+    public function getResults(): array
+    {
+        return $this->results;
     }
 }

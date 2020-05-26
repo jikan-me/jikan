1df84de1ca0b376441ae2a0c1d945b0d78f0bf79
Irfan <irfan.dahir@gmaill.com>
2020-05-26 23:20:19 +0500

    use appropriate model for club users #279

--- src/Parser/Club/ClubParser.php
+++ src/Parser/Club/ClubParser.php
@@ -7,6 +7,7 @@ use Jikan\Helper\JString;
 use Jikan\Helper\Parser;
 use Jikan\Model\Club\Club;
 use Jikan\Model\Common\MalUrl;
+use Jikan\Model\Common\UserMeta;
 use Jikan\Parser\ParserInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -279,7 +280,7 @@ class ClubParser implements ParserInterface
 
         foreach ($staffNode as $staffMember) {
             if ($staffMember instanceof Crawler) {
-                $staff[] = new MalUrl(
+                $staff[] = UserMeta::fromMeta(
                     $staffMember->text(),
                     Constants::BASE_URL . $staffMember->attr('href')
                 );

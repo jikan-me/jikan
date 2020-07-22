467d8519573feb250bd459934c0c19f7bdcbb6e1
Irfan <irfan.dahir@gmaill.com>
2020-07-22 20:42:06 +0500

    add WrapImageResource

---
+++ src/Model/Resource/WrapImageResource/Jpg.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Jikan\Model\Resource\WrapImageResource;
+
+/**
+ * Class Jpg
+ * @package Jikan\Model\Resource\WrapImageResource
+ */
+class Jpg
+{
+
+    /**
+     * @var string|null
+     */
+    private $imageUrl;
+
+    /**
+     * @param string $imageUrl
+     * @return Jpg
+     */
+    public static function factory(?string $imageUrl) : self
+    {
+        $instance = new self;
+
+        $instance->imageUrl = $imageUrl;
+
+        return $instance;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+}

42515594743ce386099806e0a12adb007f132c88
Irfan <irfan.dahir@gmaill.com>
2020-07-22 20:15:09 +0500

    add CommonImageResource

---
+++ src/Model/Resource/CommonImageResource/CommonImageResource.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Jikan\Model\Resource\CommonImageResource;
+
+/**
+ * Class CommonImageResource
+ * @package Jikan\Model\Resource\CommonImageResource
+ */
+class CommonImageResource
+{
+
+    /**
+     * @var Jpg
+     */
+    private $jpg;
+
+    /**
+     * @var Webp
+     */
+    private $webp;
+
+    /**
+     * @param string $imageUrl
+     * @return CommonImageResource
+     */
+    public static function factory(?string $imageUrl) : self
+    {
+        $instance = new self;
+
+        $instance->jpg = Jpg::factory($imageUrl);
+        $instance->webp = Webp::factory($imageUrl);
+
+        return $instance;
+    }
+
+    /**
+     * @return Jpg
+     */
+    public function getJpg(): Jpg
+    {
+        return $this->jpg;
+    }
+
+    /**
+     * @return Webp
+     */
+    public function getWebp(): Webp
+    {
+        return $this->webp;
+    }
+}
---
+++ src/Model/Resource/CommonImageResource/Webp.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Jikan\Model\Resource\CommonImageResource;
+
+/**
+ * Class Webp
+ * @package Jikan\Model\Resource\CommonImageResource
+ */
+class Webp
+{
+
+    /**
+     * @var string|null
+     */
+    private $imageUrl;
+
+    /**
+     * @var string|null
+     */
+    private $smallImageUrl;
+
+    /**
+     * @var string|null
+     */
+    private $largeImageUrl;
+
+
+    /**
+     * @param string $imageUrl
+     * @return Jpg
+     */
+    public static function factory(?string $imageUrl) : self
+    {
+        $instance = new self;
+
+        $instance->imageUrl = $imageUrl;
+
+        if ($instance->imageUrl === null) {
+            return $instance;
+        }
+
+        $instance->smallImageUrl = str_replace('.jpg', 't.webp', $imageUrl);
+        $instance->largeImageUrl = str_replace('.jpg', 'l.webp', $imageUrl);
+
+        return $instance;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSmallImageUrl(): ?string
+    {
+        return $this->smallImageUrl;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLargeImageUrl(): ?string
+    {
+        return $this->largeImageUrl;
+    }
+}

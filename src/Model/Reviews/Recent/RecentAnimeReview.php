44571b2c2a5bd3097f2c97c43df02eb629ffe518
Irfan <irfan.dahir@gmaill.com>
2021-01-14 07:38:07 +0500

    Update Reviews Model [ci skip]

--- src/Model/Reviews/Recent/RecentAnimeReview.php
+++ src/Model/Reviews/Recent/RecentAnimeReview.php
@@ -2,6 +2,7 @@
 
 namespace Jikan\Model\Reviews\Recent;
 
+use Jikan\Model\Anime\AnimeReviewScores;
 use Jikan\Model\Common\AnimeMeta;
 use Jikan\Model\Reviews\AnimeReview;
 use Jikan\Model\Reviews\Reviewer;
@@ -17,7 +18,7 @@ class RecentAnimeReview extends AnimeReview
     /**
      * @var AnimeMeta
      */
-    private $anime;
+    private $entry;
 
     /**
      * @var Reviewer
@@ -33,7 +34,7 @@ class RecentAnimeReview extends AnimeReview
     {
         $instance = new self();
 
-        $instance->anime = $parser->getAnime();
+        $instance->entry = $parser->getAnime();
         $instance->malId = $parser->getId();
         $instance->url = $parser->getUrl();
         $instance->type = $parser->getType();
@@ -45,6 +46,86 @@ class RecentAnimeReview extends AnimeReview
         $instance->episodesWatched = $parser->getEpisodesWatched();
 
         return $instance;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEpisodesWatched(): string
+    {
+        return $this->episodesWatched;
+    }
+
+    /**
+     * @return AnimeReviewScores
+     */
+    public function getScores(): AnimeReviewScores
+    {
+        return $this->scores;
+    }
+
+    /**
+     * @return AnimeMeta
+     */
+    public function getEntry(): AnimeMeta
+    {
+        return $this->entry;
+    }
+
+    /**
+     * @return Reviewer
+     */
+    public function getUser(): Reviewer
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMalId(): int
+    {
+        return $this->malId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVotes(): int
+    {
+        return $this->votes;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReview(): string
+    {
+        return $this->review;
     }
 
 }
--- src/Model/Reviews/Recent/RecentMangaReview.php
+++ src/Model/Reviews/Recent/RecentMangaReview.php
@@ -3,6 +3,7 @@
 namespace Jikan\Model\Reviews\Recent;
 
 use Jikan\Model\Common\MangaMeta;
+use Jikan\Model\Manga\MangaReviewScores;
 use Jikan\Model\Reviews\MangaReview;
 use Jikan\Model\Reviews\Reviewer;
 use Jikan\Parser\Reviews\MangaReviewParser;
@@ -17,7 +18,7 @@ class RecentMangaReview extends MangaReview
     /**
      * @var MangaMeta
      */
-    private $manga;
+    private $entry;
 
     /**
      * @var Reviewer
@@ -50,6 +51,86 @@ class RecentMangaReview extends MangaReview
         $instance->chaptersRead = $parser->getChaptersRead();
 
         return $instance;
+    }
+
+    /**
+     * @return string
+     */
+    public function getChaptersRead(): string
+    {
+        return $this->chaptersRead;
+    }
+
+    /**
+     * @return MangaReviewScores
+     */
+    public function getScores(): MangaReviewScores
+    {
+        return $this->scores;
+    }
+
+    /**
+     * @return MangaMeta
+     */
+    public function getEntry(): MangaMeta
+    {
+        return $this->entry;
+    }
+
+    /**
+     * @return Reviewer
+     */
+    public function getUser(): Reviewer
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMalId(): int
+    {
+        return $this->malId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVotes(): int
+    {
+        return $this->votes;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReview(): string
+    {
+        return $this->review;
     }
 
 }

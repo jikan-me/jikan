65e7bc9a682978bc6d3b1ffbd9e3b7de38432334
Irfan <irfan-dahir@users.noreply.github.com>
2021-07-19 16:01:30 +0500

    Fix an issue with youtube embed parsing

--- src/Helper/Media.php
+++ src/Helper/Media.php
@@ -12,27 +12,13 @@ use Jikan\Model\Resource\YoutubeImageResource;
 class Media
 {
     /**
-     * @param string $url
+     * @param string|null $url
      * @return string
      */
     public static function youtubeIdFromUrl(?string $url) : ?string
     {
         /**
-         *  https://gist.github.com/ghalusa/6c7f3a00fd2383e5ef33
-         *  Here is a sample of the URLs this regex matches:
-         * (there can be more content after the given URL that will be ignored)
-         *  http://youtu.be/dQw4w9WgXcQ
-         *  http://www.youtube.com/embed/dQw4w9WgXcQ
-         *  http://www.youtube.com/watch?v=dQw4w9WgXcQ
-         *  http://www.youtube.com/?v=dQw4w9WgXcQ
-         *  http://www.youtube.com/v/dQw4w9WgXcQ
-         *  http://www.youtube.com/e/dQw4w9WgXcQ
-         *  http://www.youtube.com/user/username#p/u/11/dQw4w9WgXcQ
-         *  http://www.youtube.com/sandalsResorts#p/c/54B8C800269D7C1B/0/dQw4w9WgXcQ
-         *  http://www.youtube.com/watch?feature=player_embedded&v=dQw4w9WgXcQ
-         *  http://www.youtube.com/?feature=player_embedded&v=dQw4w9WgXcQ
-         *  It also works on the youtube-nocookie.com URL with the same above options.
-         *  It will also pull the ID from the URL in an embed code (both iframe and object tags)
+         *  https://stackoverflow.com/a/17030234
          */
 
         if ($url === null) {
@@ -40,12 +26,12 @@ class Media
         }
 
         preg_match(
-            '%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i',
+            "/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/",
             $url,
-            $match
+            $matches
         );
 
-        return $match[1];
+        return $matches[1] ?? null;
     }
 
     /**

69356cc9b8909c3e356552e79f1e4421e8898f01
Irfan <irfan-dahir@users.noreply.github.com>
2021-09-11 02:19:22 +0500

    character pictures - return only default size #359 2.x [ci skip]

---
+++ src/Model/Common/DefaultPicture.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Jikan\Model\Common;
+
+use Jikan\Parser\Common\PictureParser;
+
+/**
+ * Class DefaultPicture
+ *
+ * @package Jikan\Model
+ */
+class DefaultPicture
+{
+    /**
+     * @var string
+     */
+    private $imageUrl;
+
+    /**
+     * @param PictureParser $parser
+     *
+     * @return DefaultPicture
+     * @throws \InvalidArgumentException
+     */
+    public static function fromParser(PictureParser $parser): DefaultPicture
+    {
+        $instance = new self();
+        $instance->imageUrl = $parser->getSmall();
+
+        return $instance;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageUrl(): string
+    {
+        return $this->imageUrl;
+    }
+
+}
---
+++ src/Parser/Common/DefaultPicturesPageParser.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Jikan\Parser\Common;
+
+use Jikan\Model\Common\DefaultPicture;
+use Jikan\Parser\ParserInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Class DefaultPicturesPageParser
+ *
+ * Character pictures do not have any large image sizes,
+ * so this class returns only the default size
+ *
+ * @package Jikan\Parser\Common
+ */
+class DefaultPicturesPageParser implements ParserInterface
+{
+    /**
+     * @var Crawler
+     */
+    private $crawler;
+
+    /**
+     * DefaultPicturesPageParser constructor.
+     *
+     * @param Crawler $crawler
+     */
+    public function __construct(Crawler $crawler)
+    {
+        $this->crawler = $crawler;
+    }
+
+    /**
+     * @return \Jikan\Model\Common\DefaultPicture[]
+     * @throws \InvalidArgumentException
+     */
+    public function getModel(): array
+    {
+        return $this->crawler
+            ->filterXPath('//a[@class="js-picture-gallery"]')
+            ->each(
+                function (Crawler $crawler) {
+                    return DefaultPicture::fromParser(new PictureParser($crawler));
+                }
+            );
+    }
+}

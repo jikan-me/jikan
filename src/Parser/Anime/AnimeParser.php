f055d9f0a89affbcffd1b78e20b1157db6e62989
Irfan <irfan-dahir@users.noreply.github.com>
2021-10-01 18:54:54 +0500

    fix #365 for v2  [ci skip]

--- src/Parser/Anime/AnimeParser.php
+++ src/Parser/Anime/AnimeParser.php
@@ -723,9 +723,9 @@ class AnimeParser implements ParserInterface
      */
     public function getOpeningThemes(): array
     {
-        return $this->crawler->filterXPath('//div[@class="theme-songs js-theme-songs opnening"]//span[@class="theme-song"]')
+        return $this->crawler->filterXPath('//div[@class="theme-songs js-theme-songs opnening"]/table/tr')
             ->each(function (Crawler $crawler) {
-                return $crawler->text();
+                return JString::cleanse($crawler->text());
             });
     }
 
@@ -736,9 +736,9 @@ class AnimeParser implements ParserInterface
      */
     public function getEndingThemes(): array
     {
-        return $this->crawler->filterXPath('//div[@class="theme-songs js-theme-songs ending"]//span[@class="theme-song"]')
+        return $this->crawler->filterXPath('//div[@class="theme-songs js-theme-songs ending"]/table/tr')
             ->each(function (Crawler $crawler) {
-                return $crawler->text();
+                return JString::cleanse($crawler->text());
             });
     }
 

dacf1f486143ecacec6c887a7a879519932d83c2
Irfan <irfan-dahir@users.noreply.github.com>
2021-11-13 03:18:20 +0500

    v3: update constants

--- src/Helper/Constants.php
+++ src/Helper/Constants.php
@@ -47,23 +47,9 @@ class Constants
     public const TOP_BY_POPULARITY = 'bypopularity';
     public const TOP_BY_FAVORITES = 'favorite';
 
-    public const RECENT_REVIEW_ANIME = 'anime';
-    public const RECENT_REVIEW_MANGA = 'manga';
-    public const RECENT_REVIEW_BEST_VOTED = 'bestvoted';
-
-    public const RECENT_RECOMMENDATION_ANIME = 'anime';
-    public const RECENT_RECOMMENDATION_MANGA = 'manga';
-
-    // v3 status const // remove old ones
-    public const STATUS_ANIME_AIRING = 1;
-    public const STATUS_ANIME_FINISHED = 2;
-    public const STATUS_ANIME_NOT_YET_AIRED = 3;
-    public const STATUS_MANGA_PUBLISHING = 1;
-    public const STATUS_MANGA_FINISHED = 2;
-    public const STATUS_MANGA_NOT_YET_PUBLISHED = 3;
-    public const STATUS_MANGA_ON_HIATUS = 4;
-    public const STATUS_MANGA_DISCONTINUED = 5;
-
+    public const TOP_REVIEW_ANIME = 'anime';
+    public const TOP_REVIEW_MANGA = 'manga';
+    public const TOP_REVIEW_BEST_VOTED = 'bestvoted';
 
     public const SEARCH_ANIME_TV = 1;
     public const SEARCH_ANIME_OVA = 2;
@@ -82,7 +68,7 @@ class Constants
     public const SEARCH_ANIME_STATUS_AIRING = 1;
     public const SEARCH_ANIME_STATUS_FINISHED_AIRING = 2;
     public const SEARCH_ANIME_STATUS_COMPLETED = 2; // alias
-    public const SEARCH_ANIME_STATUS_TO_BE_AIRED = 3;
+    public const SEARCH_ANIME_STATUS_TO_BE_AIRD = 3;
     public const SEARCH_ANIME_STATUS_TBA = 3; // alias
 
     public const SEARCH_MANGA_STATUS_PUBLISHING = 1;
@@ -128,16 +114,12 @@ class Constants
     public const SEARCH_MANGA_ORDER_BY_TYPE = 8; // Default: Manhua, Manhwa, Doujinshi, One-shot, Novel, Manga
     public const SEARCH_MANGA_ORDER_BY_ID = 9;
 
-    public const SEARCH_USER_GENDER_ANY = -1;
-    public const SEARCH_USER_GENDER_MALE = 1;
-    public const SEARCH_USER_GENDER_FEMALE = 2;
-    public const SEARCH_USER_GENDER_NONBINARY = 3;
-
     public const GENRE_ANIME_ACTION = 1;
     public const GENRE_ANIME_ADVENTURE = 2;
     public const GENRE_ANIME_CARS = 3;
     public const GENRE_ANIME_COMEDY = 4;
-    public const GENRE_ANIME_DEMENTIA = 5;
+    public const GENRE_ANIME_AVANT_GARDE = 5;
+    public const GENRE_ANIME_DEMENTIA = 5; // Renamed to the above by MAL
     public const GENRE_ANIME_DEMONS = 6;
     public const GENRE_ANIME_MYSTERY = 7;
     public const GENRE_ANIME_DRAMA = 8;
@@ -148,7 +130,7 @@ class Constants
     public const GENRE_ANIME_HISTORICAL = 13;
     public const GENRE_ANIME_HORROR = 14;
     public const GENRE_ANIME_KIDS = 15;
-    public const GENRE_ANIME_MAGIC = 16;
+    public const GENRE_ANIME_MAGIC = 16; // Removed by MAL - will throw BadResponseException (404)
     public const GENRE_ANIME_MARTIAL_ARTS = 17;
     public const GENRE_ANIME_MECHA = 18;
     public const GENRE_ANIME_MUSIC = 19;
@@ -158,30 +140,38 @@ class Constants
     public const GENRE_ANIME_SCHOOL = 23;
     public const GENRE_ANIME_SCI_FI = 24;
     public const GENRE_ANIME_SHOUJO = 25;
-    public const GENRE_ANIME_SHOUJO_AI = 26;
+    public const GENRE_ANIME_GIRLS_LOVE = 26;
+    public const GENRE_ANIME_SHOUJO_AI = 26; // Renamed to the above by MAL
     public const GENRE_ANIME_SHOUNEN = 27;
-    public const GENRE_ANIME_SHOUNEN_AI = 28;
+    public const GENRE_ANIME_BOYS_LOVE = 28;
+    public const GENRE_ANIME_SHOUNEN_AI = 28; // Renamed to the above by MAL
     public const GENRE_ANIME_SPACE = 29;
     public const GENRE_ANIME_SPORTS = 30;
     public const GENRE_ANIME_SUPER_POWER = 31;
     public const GENRE_ANIME_VAMPIRE = 32;
-    public const GENRE_ANIME_YAOI = 33;
-    public const GENRE_ANIME_YURI = 34;
+    public const GENRE_ANIME_YAOI = 33; // Merged into GENRE_ANIME_BOYS_LOVE by MAL - will throw BadResponseException (404)
+    public const GENRE_ANIME_YURI = 34; // Merged in GENRE_ANIME_GIRLS_LOVE by MAL - will throw BadResponseException (404)
     public const GENRE_ANIME_HAREM = 35;
     public const GENRE_ANIME_SLICE_OF_LIFE = 36;
     public const GENRE_ANIME_SUPERNATURAL = 37;
     public const GENRE_ANIME_MILITARY = 38;
     public const GENRE_ANIME_POLICE = 39;
     public const GENRE_ANIME_PSYCHOLOGICAL = 40;
-    public const GENRE_ANIME_THRILLER = 41;
+    public const GENRE_ANIME_SUSPENSE = 41;
+    public const GENRE_ANIME_THRILLER = 41; // Renamed to the above by MAL
     public const GENRE_ANIME_SEINEN = 42;
     public const GENRE_ANIME_JOSEI = 43;
+    public const GENRE_ANIME_AWARD_WINNING = 46;
+    public const GENRE_ANIME_GOURMET = 47;
+    public const GENRE_ANIME_WORK_LIFE = 48;
+    public const GENRE_ANIME_EROTICA = 49;
 
     public const GENRE_MANGA_ACTION = 1;
     public const GENRE_MANGA_ADVENTURE = 2;
     public const GENRE_MANGA_CARS = 3;
     public const GENRE_MANGA_COMEDY = 4;
-    public const GENRE_MANGA_DEMENTIA = 5;
+    public const GENRE_MANGA_DEMENTIA = 5; // Renamed to the above by MAL
+    public const GENRE_MANGA_AVANT_GARDE = 5;
     public const GENRE_MANGA_DEMONS = 6;
     public const GENRE_MANGA_MYSTERY = 7;
     public const GENRE_MANGA_DRAMA = 8;
@@ -192,7 +182,7 @@ class Constants
     public const GENRE_MANGA_HISTORICAL = 13;
     public const GENRE_MANGA_HORROR = 14;
     public const GENRE_MANGA_KIDS = 15;
-    public const GENRE_MANGA_MAGIC = 16;
+    public const GENRE_MANGA_MAGIC = 16; // Removed by MAL - will throw BadResponseException (404)
     public const GENRE_MANGA_MARTIAL_ARTS = 17;
     public const GENRE_MANGA_MECHA = 18;
     public const GENRE_MANGA_MUSIC = 19;
@@ -202,15 +192,17 @@ class Constants
     public const GENRE_MANGA_SCHOOL = 23;
     public const GENRE_MANGA_SCI_FI = 24;
     public const GENRE_MANGA_SHOUJO = 25;
-    public const GENRE_MANGA_SHOUJO_AI = 26;
+    public const GENRE_MANGA_GIRLS_LOVE = 26;
+    public const GENRE_MANGA_SHOUJO_AI = 26; // Renamed to the above by MAL
     public const GENRE_MANGA_SHOUNEN = 27;
-    public const GENRE_MANGA_SHOUNEN_AI = 28;
+    public const GENRE_MANGA_BOYS_LOVE = 28;
+    public const GENRE_MANGA_SHOUNEN_AI = 28; // Renamed to the above by MAL
     public const GENRE_MANGA_SPACE = 29;
     public const GENRE_MANGA_SPORTS = 30;
     public const GENRE_MANGA_SUPER_POWER = 31;
     public const GENRE_MANGA_VAMPIRE = 32;
-    public const GENRE_MANGA_YAOI = 33;
-    public const GENRE_MANGA_YURI = 34;
+    public const GENRE_MANGA_YAOI = 33; // Merged into GENRE_MANGA_BOYS_LOVE by MAL - will throw BadResponseException (404)
+    public const GENRE_MANGA_YURI = 34; // Merged into GENRE_MANGA_GIRLS_LOVE by MAL - will throw BadResponseException (404)
     public const GENRE_MANGA_HAREM = 35;
     public const GENRE_MANGA_SLICE_OF_LIFE = 36;
     public const GENRE_MANGA_SUPERNATURAL = 37;
@@ -221,7 +213,12 @@ class Constants
     public const GENRE_MANGA_JOSEI = 42;
     public const GENRE_MANGA_DOUJINSHI = 43;
     public const GENRE_MANGA_GENDER_BENDER = 44;
-    public const GENRE_MANGA_THRILLER = 45;
+    public const GENRE_MANGA_SUSPENSE = 45;
+    public const GENRE_MANGA_THRILLER = 45; // Renamed to the above by MAL
+    public const GENRE_MANGA_AWARD_WINNING = 46;
+    public const GENRE_MANGA_GOURMET = 47;
+    public const GENRE_MANGA_WORK_LIFE = 48;
+    public const GENRE_MANGA_EROTICA = 49;
 
     /*
      * User List Ordering Explained
@@ -280,6 +277,8 @@ class Constants
     public const USER_MANGA_LIST_CURRENTLY_PUBLISHING = 1;
     public const USER_MANGA_LIST_FINISHED_PUBLISHING = 2;
     public const USER_MANGA_LIST_NOT_YET_PUBLISHED = 3;
+    public const USER_MANGA_LIST_ON_HIATUS = 4;
+    public const USER_MANGA_LIST_DISCONTINUED = 5;
 
     public const USER_LIST_SORT_DESCENDING = 1;
     public const USER_LIST_SORT_ASCENDING = -1;

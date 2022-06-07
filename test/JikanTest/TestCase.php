ace96286643ec0a993bfdd8aa73fc576a923b492
Irfan <irfan-dahir@users.noreply.github.com>
2022-06-07 23:32:44 +0500

    update test cache dir to vendor fixtures

--- test/JikanTest/TestCase.php
+++ test/JikanTest/TestCase.php
@@ -17,7 +17,10 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
         parent::setUp();
 
         $testClass = str_replace('\\', '_', get_class($this));
-        $cachePath = join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'http.cache', $testClass]);
+        $cachePath = join(
+            DIRECTORY_SEPARATOR,
+            [__DIR__, '..', '..', 'vendor','jikan-me', 'jikan-fixtures', 'fixtures', $testClass]
+        );
         $this->httpClient = new MockHttpClient(new HttpFilesystemCacheClientCallback($cachePath));
     }
 }

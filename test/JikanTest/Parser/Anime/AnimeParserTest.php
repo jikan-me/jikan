eaa274b18822dc474a606a6cd671ace243ab76a2
ToshY <31921460+ToshY@users.noreply.github.com>
2022-11-06 20:20:52 +0100

    Change anime synopsis xpath and updated related test

--- test/JikanTest/Parser/Anime/AnimeParserTest.php
+++ test/JikanTest/Parser/Anime/AnimeParserTest.php
@@ -107,8 +107,14 @@ class AnimeParserTest extends TestCase
      */
     public function it_gets_the_anime_synopsis(): void
     {
-        self::assertStringContainsString(
-            'Vash the Stampede is the man with a $$60,000,000,000 bounty on his head.',
+        self::assertEquals(
+            <<<EOT
+            Vash the Stampede is the man with a $$60,000,000,000 bounty on his head. The reason: he's a merciless villain who lays waste to all those that oppose him and flattens entire cities for fun, garnering him the title "The Humanoid Typhoon." He leaves a trail of death and destruction wherever he goes, and anyone can count themselves dead if they so much as make eye contact—or so the rumors say. In actuality, Vash is a huge softie who claims to have never taken a life and avoids violence at all costs.
+
+            With his crazy doughnut obsession and buffoonish attitude in tow, Vash traverses the wasteland of the planet Gunsmoke, all the while followed by two insurance agents, Meryl Stryfe and Milly Thompson, who attempt to minimize his impact on the public. But soon, their misadventures evolve into life-or-death situations as a group of legendary assassins are summoned to bring about suffering to the trio. Vash's agonizing past will be unraveled and his morality and principles pushed to the breaking point.
+
+            [Written by MAL Rewrite]
+            EOT,
             $this->parser->getSynopsis()
         );
     }

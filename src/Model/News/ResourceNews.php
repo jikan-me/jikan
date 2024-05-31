e4e722cf47247a1b0d4acee225f52ed731d7c490
Irfan <irfan-dahir@users.noreply.github.com>
2024-05-31 12:39:35 +0500

    Update ResourceNews.php

--- src/Model/News/ResourceNews.php
+++ src/Model/News/ResourceNews.php
@@ -83,6 +83,7 @@ class ResourceNews
     public static function fromParser(NewsParser $parser): self
     {
         $instance = new self();
+
         $instance->malId = $parser->getMalId();
         $instance->url = $parser->getUrl();
         $instance->title = $parser->getTitle();
@@ -98,5 +99,109 @@ class ResourceNews
         $instance->relatedNews = $parser->getRelatedNews();
 
         return $instance;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getMalId(): ?int
+    {
+        return $this->malId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorUsername(): string
+    {
+        return $this->authorUsername;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorUrl(): string
+    {
+        return $this->authorUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getForumUrl(): string
+    {
+        return $this->forumUrl;
+    }
+
+    /**
+     * @return WrapImageResource
+     */
+    public function getImages(): WrapImageResource
+    {
+        return $this->images;
+    }
+
+    /**
+     * @return int
+     */
+    public function getComments(): int
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTags(): array
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRelatedEntries(): array
+    {
+        return $this->relatedEntries;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRelatedNews(): array
+    {
+        return $this->relatedNews;
     }
 }
